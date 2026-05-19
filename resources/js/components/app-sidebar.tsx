49f0b07819b4f4006719e152c00908dfd66b613d
cakmalik <hasinilmalik@gmail.com>
2026-05-20 00:16:18 +0700

    remove sidebar footer

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -149,7 +149,9 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
+                {/*
+                  <NavFooter items={footerNavItems} className="mt-auto" />
+*/}
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
